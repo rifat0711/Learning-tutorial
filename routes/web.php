f2631ba4ebd3613ab1196ff27ee1cbeb9356f899
rifat0711 <57683016+rifat0711@users.noreply.github.com>
2023-10-07 10:34:42 +0600

    Upload

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    
+    return view('welcome');
+});
+
+
+Route::get('/about',function(){
+    return view('about');
+    
+})->name('about.us');
+
+//Route::view('/about','about');
+
+Route::get(md5('/contact'),function(){
+    return view('contact');
+    //return "My roll is  $roll";
+})->name('contact.us');
+
+
+Route::get('/country',function(){
+    return view('country');
+    
+})->middleware('country');
+
+//__ignore it__ //
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
